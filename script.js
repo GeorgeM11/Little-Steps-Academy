a0c93e09da7d1088bf02051190d6574fe301c56d
GeorgeM11 <97505972+GeorgeM11@users.noreply.github.com>
2025-10-16 12:40:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -171,3 +171,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.querySelector('.hero');
+  hero.classList.add('animate');
+});
